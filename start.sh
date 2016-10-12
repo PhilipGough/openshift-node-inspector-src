7074713a133cecc43fc062e67809c4d20e7d1e7a
Philip Gough <pgough@redhat.com>
2016-10-12 10:55:34 +0100

    Add port as env var

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Starting Node Inspector Server"
-node /tmp/openshift-node-inspector-src/bin/inspector.js -p 9000 & 
+node /tmp/openshift-node-inspector-src/bin/inspector.js -p ${ONI_DEBUG_PORT} & 
 sleep 5 
 echo "Starting ${ONI_COMPONENT}"
 node --debug /opt/app-root/src/${ONI_COMPONENT}.js /opt/app-root/src/config/conf.json --master-only
