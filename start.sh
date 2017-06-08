d9b98caa3da074a37a053bd6fd994754d367bca0
Laura Fitzgerald <lfitzger@redhat.com>
2017-06-08 12:05:55 +0100

    ngui start

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ echo "Starting Node Inspector Server"
 node /tmp/openshift-node-inspector-src/bin/inspector.js -p ${ONI_DEBUG_PORT} & 
 sleep 5 
 echo "Starting ${ONI_COMPONENT}"
-node --debug /opt/app-root/src/${ONI_COMPONENT}.js /opt/app-root/src/config/conf.json --master-only
+node --debug app.js ./config/conf.json --master-only
